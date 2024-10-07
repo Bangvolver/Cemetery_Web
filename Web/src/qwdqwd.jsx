d4ac073d8980545efe20752db8a70acb304493dd
jeri <jerimonski@gmail.com>
2024-10-07 08:33:17 -0300

    HeaderMigrado

--- Web/src/qwdqwd.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Button({txtButton}) {
-    return (
-        <button>{txtButton}</button>
-    )
-}
-
-export default Button
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import React from 'react'
-import Buttons from './components/Buttons'
+import Header from './Header'
 
 createRoot(document.getElementById('root')).render(
   <>
-    <Buttons txtButton="mamamia"/>
-    <Button>ascsacs</Button>
+      <Header/>
   </>
 )
+
