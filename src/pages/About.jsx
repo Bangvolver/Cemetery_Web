18ab30a7f9d9f34a0ad83316f2b1776adcd05527
jeri <jerimonski@gmail.com>
2024-10-20 20:40:04 -0300

    Style: Implementacion de nuevas secciones en about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import { styled } from 'styled-components';
 import Icons from './../components/ServicesIconsSvg/ServicesIcons';
+import ProfilesImagenes from './../components/ProfilesImg/ProfilesImagenes';
 
 function About() {
     return (
@@ -34,13 +35,13 @@ function About() {
                         experiencia significativa y cercana que ayuda a las familias a mantener vivo el legado de sus
                         seres queridos.
                     </p>
-                    <img className='MainImg' src="https://i.pinimg.com/564x/fb/50/a1/fb50a1805ea2b43cad75a364b643bee9.jpg" alt="" />
                 </div>
             </section>
             <section className="ServicesEcos">
                 <div className="icons">
                     <Icons type="organization" />
                     <p>
+                        <h3>Organizacion</h3>
                         Servicios de planificación anticipada, 
                         pueden expresar y organizar sus deseos 
                         y preferencias para su despedida.
@@ -49,6 +50,7 @@ function About() {
                 <div className="icons">
                     <Icons type="ceremony" />
                     <p>
+                        <h3>Ceremonias</h3>
                         Diseñamos funerales según las preferencias
                         de cada familia para una despedida significativa.
                     </p>
@@ -56,9 +58,25 @@ function About() {
                 <div className="icons">
                     <Icons type="cloudStorage" />
                     <p>
+                        <h3>Digitalizacion</h3>
                         Ofrecemos un espacio en la nube para recordar
                         y honrar a los difuntos desde cualquier lugar.
                     </p>
+                </div>
+            </section>
+            <section className='OurTeam'>
+                <h1>Nuestro equipo</h1>
+                <div className='OurTeamContainer'>
+                    <ProfilesImagenes 
+                        LinkImg={"https://cdns-images.dzcdn.net/images/artist/89763ed244635130826aa4ce159826f5/1900x1900-000000-80-0-0.jpg"} 
+                        Nombre={"Jeremynho Deus"} 
+                        Cargo={"Lavaplatos"} 
+                    />
+                    <ProfilesImagenes 
+                        LinkImg={"https://www.pockettactics.com/wp-content/sites/pockettactics/2024/07/Zenless-Zone-Zero-Anby-1.jpg"} 
+                        Nombre={"Shirailinha Kos "} 
+                        Cargo={"Atrapa Ratones"} 
+                    />
                 </div>
             </section>
         </SectionAbout>
@@ -103,14 +121,14 @@ const SectionAbout = styled.section`
         color: rgb(254, 249, 195);
         background-color: rgb(202, 138, 4);
         p {
-            width: 700px;
+            max-width: 700px;
             display: flex;
             flex-direction: column;
             text-align: left;
             font-size: 1.2rem;
             margin: 0 0 20px 0;
             span{
-                font-size: 2rem;
+                font-size: 3rem;
                 margin-bottom: 20px;
                 font-weight: bolder;
             }
@@ -133,10 +151,33 @@ const SectionAbout = styled.section`
     .icons{
         display: flex;
         flex-direction: row;
+        align-items: center;
         p{
-            width: 200px;
+            max-width: 200px;
             margin-left: 2rem;
             color: rgb(133, 77, 14);
+            h3{
+                margin: 0 0 10px 0;
+            }
         }
+    }
+
+    .OurTeam{
+        text-align: center;
+        background-color: rgb(202, 138, 4);
+            h1{
+            padding: 4rem;
+            font-size: 3rem;
+            font-weight: bolder;
+            margin: 0;
+        }
+    }
+    .OurTeamContainer{
+        width: 100%;
+        height: auto;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        text-align: center;
     }
 `
