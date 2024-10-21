65b54a63e48cfc51fbfefae489370e4af49a5cf4
Bangvolver <alikatormes@gmail.com>
2024-10-20 22:21:52 -0300

    Era de prueba sorry

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,25 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
+import styled from 'styled-components' 
 
 function Footer() {
     return (
-        <footer>
-            <div>JIJIJIJII</div>   
-        </footer>
+        <FooterContainer> 
+            <div className='marijuan'>JIJIJIJII</div>  
+ 
+        </FooterContainer> //dentro del FooterContainer podes escribir Html.
     )
+
 }
 
-export default Footer
+ //aqui fiera de funcion puede Js. (Arriba como abajo)
+
+export default Footer 
+const FooterContainer = styled.footer ` // FooterCointer = FootercContainer(RED inside return) & de aqui pa bajo CSS
+.marijuan{
+    width: 100px;
+    color: #000;
+    height: 100px;
+    background-color: #000;
+}
+`
